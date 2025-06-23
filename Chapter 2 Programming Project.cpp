19f344c37315c75b33af904f02687dea4e4842d9
RScott99 <rsh06.rh@gmail.com>
2025-06-23 17:33:38 -0400

    Completed the code for the distance per tank of gas program

--- Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project.cpp
@@ -5,6 +5,19 @@
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Defining variables
+    int tank;
+    double tMpg, hMpg; //tmpg = town miles per gallon, hmpg = highway miles per gallon
+    double tDist, hDist; //tDist = tow distance, hDist = highway distance
+    //Initializing the tank capacity and gas efficiency
+    tank = 20;
+    tMpg = 23.5;
+    hMpg = 28.9;
+    //Calculating distance for both conditions
+    tDist = tank * tMpg;
+    hDist = tank * hMpg;
+    //Output distances
+    std::cout << "In town, the car can travel " << tDist << " miles on one tank." << endl;
+    std::cout << "On the highway, the car can travel " << hDist << " miles on one tank." << endl;
 }
 
