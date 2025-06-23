0bcdf5a7c869e003ae9b7ae268c86493cdd6d22b
RScott99 <rsh06.rh@gmail.com>
2025-06-23 17:09:50 -0400

    Written Code for the Ocean Level Program.

--- Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project.cpp
@@ -5,6 +5,16 @@
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    //need variables for each year as integers and the rate the ocean rises as a double
+    double fiveYear, sevenYear, tenYear;
+    double riseRate;
+    //initializing each variable
+    riseRate = 1.5;
+    fiveYear = 5 * riseRate;
+    sevenYear = 7 * riseRate;
+    tenYear = 10 * riseRate;
+    std::cout << "In five years, the ocean level has risen " << fiveYear << "mm." << endl;
+    std::cout << "In seven years, the ocean level has risen " << sevenYear << "mm." << endl;
+    std::cout << "In ten years, the ocean level has risen " << tenYear << "mm." << endl;
 }
 
