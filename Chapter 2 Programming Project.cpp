23b0c1d62079dbc0cda8880408ba9220aa81a47f
RScott99 <rsh06.rh@gmail.com>
2025-06-23 17:59:56 -0400

    Completed Energy Drink Consumption program

--- Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project.cpp
@@ -5,6 +5,17 @@
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Defining variables
+    int customers, oneOrMore, citrusFlavored;
+    //Initializing variables
+    customers = 16500; //Total number of Customers surveyed
+    oneOrMore = 15; //Percent of people who purchace one or more drinks per week
+    citrusFlavored = 58; //Percent of people who prefer citrus-flavored energy drinks
+    //Calculating how many people make up each percentage
+    double oneOrMorePer = (customers * oneOrMore) / 100;
+    double citrusFlavoredPer = (customers * citrusFlavored) / 100;
+    //Outputting Results
+    std::cout << "Approximately " << oneOrMorePer << " of their customers surveyed purchace one or more energy drinks per week." << endl;
+    std::cout << "Approximately " << citrusFlavoredPer << " of their customers surveyed prefer citrus - flavored energy drinks." << endl;
 }
 
